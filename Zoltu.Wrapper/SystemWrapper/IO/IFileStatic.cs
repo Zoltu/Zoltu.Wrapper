cf0dbcc665f9501892415e6853dcdb1aba5b4af5
Micah <micah@zoltu.net>
2015-01-01 19:57:31 -0800

    Adds ReadAllText overload with Encoding.

--- Zoltu.Wrapper/SystemWrapper/IO/IFileStatic.cs
+++ Zoltu.Wrapper/SystemWrapper/IO/IFileStatic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Text;
 
 namespace Zoltu.Wrapper.SystemWrapper.IO
 {
@@ -8,6 +9,7 @@ namespace Zoltu.Wrapper.SystemWrapper.IO
 	public interface IFileStatic
 	{
 		String ReadAllText(String filePath);
+		String ReadAllText(String filePath, Encoding encoding);
 	}
 
 	[ContractClassFor(typeof(IFileStatic))]
@@ -21,6 +23,16 @@ namespace Zoltu.Wrapper.SystemWrapper.IO
 
 			return default (String);
 		}
+
+		public String ReadAllText(String filePath, Encoding encoding)
+		{
+			Contract.Requires<ArgumentNullException>(filePath != null);
+			Contract.Requires<ArgumentException>(filePath.Length != 0);
+			Contract.Requires<ArgumentNullException>(encoding != null);
+			Contract.Ensures(Contract.Result<String>() != null);
+
+			return default (String);
+		}
 	}
 
 	public class FileStaticWrapper : IFileStatic
@@ -28,6 +40,11 @@ namespace Zoltu.Wrapper.SystemWrapper.IO
 		public String ReadAllText(String filePath)
 		{
 			return File.ReadAllText(filePath);
+		}
+
+		public String ReadAllText(String filePath, Encoding encoding)
+		{
+			return File.ReadAllText(filePath, encoding);
 		}
 	}
 }
